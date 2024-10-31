cff35d92517477495edcf46263a7886363e656e5
Francisco Victor da Silva Pinheiro <37351953+pinheirovictor@users.noreply.github.com>
2024-10-31 07:59:12 -0300

    Delete Códigos aula 25-10 directory

--- Códigos aula 25-10/t3.py
+++
@@ -1,6 +0,0 @@
-# Abrindo o arquivo que pode conter uma BOM, utilizando 'utf-8-sig' para lidar com a BOM automaticamente
-with open('arquivo_com_bom.txt', 'r', encoding='utf-8-sig') as file:
-    conteudo = file.read()
-
-# Exibe o conteúdo do arquivo, sem a BOM
-print(conteudo)
--- Códigos aula 25-10/t8.py
+++
@@ -1,4 +0,0 @@
-# Abrindo o arquivo para escrita
-with open('saida.txt', 'w', encoding='utf-8') as file:
-    # Escreve "Java" no arquivo
-    file.write("Java\n")
